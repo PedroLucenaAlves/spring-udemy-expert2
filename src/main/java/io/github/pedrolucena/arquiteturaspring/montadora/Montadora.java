621ae0198225030265a629081606c4d46687395d
PedroLucenaAlves <alvespedroalonso@gmail.com>
2025-07-26 17:47:50 -0300

    first commit

---
+++ src/main/java/io/github/pedrolucena/arquiteturaspring/montadora/Montadora.java
@@ -0,0 +1,6 @@
+package io.github.pedrolucena.arquiteturaspring.montadora;
+
+public enum Montadora {
+    HONDA,
+    TOYOTA
+}
---
+++ src/main/java/io/github/pedrolucena/arquiteturaspring/montadora/TipoMotor.java
@@ -0,0 +1,9 @@
+package io.github.pedrolucena.arquiteturaspring.montadora;
+
+public enum TipoMotor {
+
+    ASPIRADO,
+    TURBO,
+    ELETRICO
+
+}
